76bba826bb184c13bc84353a6103126c612acdc8
Jakob Hendrix <jhendrix001@gmail.com>
2023-03-09 17:01:54 -0600

    Added basic models and table view of statuses

---
+++ BuffTracker/BuffTracker.App/Models/StatusEffect.cs
@@ -0,0 +1,10 @@
+﻿namespace BuffTracker.App.Models
+{
+    public class StatusEffect
+    {
+        public string Name { get; set; }
+        public int MaxDurationInRounds { get; set; }
+        public int RoundWhenCast { get; set; }
+        public int RemainingRounds { get; }
+    }
+}
